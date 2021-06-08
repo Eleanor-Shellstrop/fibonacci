0cbb15b0dc1745818e92f44ca6f34838b8e66bf1
Anne Ensign <anne.ensign@gmail.com>
2021-06-08 12:13:00 -0400

    Add form reset

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let fibArray = [];
 let fibDiv = [];
 
 function fibonacciArray (numberInput, array) {
+    if (numberInput > 25) {
+        alert("Number too large, enter number less than 25");
+    }
     for (let i = 0; i < numberInput; i++) {
         let firstNum = 1;
         let secondNum = 0;
@@ -27,8 +30,8 @@ function fibonacciArray (numberInput, array) {
 function recursiveFibonacci (x) {
     if (x <= 1) {
         return 1;
-    } else if (x > 100) {
-        alert("Number too large, enter number less than 100");
+    } else if (x > 25) {
+        alert("Number too large, enter number less than 25");
     } else {
         return recursiveFibonacci(x - 1) + recursiveFibonacci(x -2);
     }
@@ -38,23 +41,27 @@ function recursiveFibonacci (x) {
 //  -----------------------------------------------------------------------------
 // Input calculations
 
-const recursiveInput = document.getElementById("recursive_input").value;
+const recursiveInput = document.getElementById('recursive_input').value;
 const recursiveIndex = parseInt(recursiveInput);
 const arrayButton = document.getElementById('array_button');
 const recursiveButton = document.getElementById('recursive_button');
+const resultDiv = document.getElementById('result');
+const form = document.getElementById('form');
 
 arrayButton.addEventListener('click', () => {
-    const arrayInput = document.getElementById("array_input").value - 1;
+    const arrayInput = document.getElementById('array_input').value - 1;
     const arrayIndex = parseInt(arrayInput);
     fibonacciArray(arrayIndex, fibArray);
-    console.log(fibArray.pop());
+    result.innerHTML = fibArray.pop();
+    form.reset();
 });
 
 
 recursiveButton.addEventListener('click', () => {
-    const recursiveInput = document.getElementById("recursive_input").value -1;
+    const recursiveInput = document.getElementById('recursive_input').value -1;
     const recursiveIndex = parseInt(recursiveInput);
-    console.log(recursiveFibonacci(recursiveIndex));
+    result.innerHTML = recursiveFibonacci(recursiveIndex);
+    form.reset();
 });
 
 
@@ -62,11 +69,11 @@ recursiveButton.addEventListener('click', () => {
 //  -----------------------------------------------------------------------------
 // Fill Div with Sequence
 function fillFibonacciDiv () {
-    fibonacciArray(25, fibDiv);
+    fibonacciArray(24, fibDiv);
     for (let i = 0; i < fibDiv.length; i++) {
        let li = document.createElement('li');
        li.innerText = fibDiv[i];
-        document.getElementById("fib_numbers").appendChild(li); 
+        document.getElementById('fib_numbers').appendChild(li); 
     }
 }
 
